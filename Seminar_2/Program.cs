d8dee73177a4a946f7f314fea1552b84a019bde5
Komparon <one77@mail.ru>
2022-10-17 21:32:07 +0300

    задача 11

--- Seminar_2/Program.cs
+++ Seminar_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98 
+
+*/
+
+
+
+// Случайное число
+int number = new Random().Next(100, 999);
+int a1 = number / 100;
+int a2 = number % 10;
+
+Console.WriteLine(number);
+int  result = a1*10 + a2;
+Console.Write(result);
