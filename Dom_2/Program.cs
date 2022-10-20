e1e477792743809a34df9a0539c78b6272bef8f6
Komparon <one77@mail.ru>
2022-10-20 14:11:12 +0300

    Домашнее задание №2

--- Dom_2/Program.cs
+++ Dom_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.Write("Введите трехзначное число:...");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int a2 = number % 100;
+int a1 = a2 /10;
+
+Console.WriteLine(a1);
