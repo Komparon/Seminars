c9806c7e56d8abda075e8267c4ec209bf83d7520
Komparon <one77@mail.ru>
2022-11-17 17:32:03 +0300

    занятия на 6 семинаре

--- Seminar_6/Program.cs
+++ Seminar_6/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон
+*/
+bool CheckTriangle(int x, int y, int z)
+{
+    if ((x < y + z) && (y < x + z) && (z < x + y))
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+Console.WriteLine("Введите x ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+
+bool rez = CheckTriangle(x, y, z);
+Console.WriteLine(rez);
