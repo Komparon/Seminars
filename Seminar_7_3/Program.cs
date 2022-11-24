a0835cea924415b4ee587dbf83681ff330c56652
Komparon <one77@mail.ru>
2022-11-24 19:05:46 +0300

    Семинары 7 и 8 и рабочие задания рассмотренные на них

---
+++ Seminar_7_3/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+
+Console.Clear();
+Console.WriteLine("Сколько строк?");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сколько столбцов? А ну да, зачем спрашиваю. Столько же будет.");
+int n = m;
+int[,] array = new int[m, n];
+int[,] FillArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return arr;
+}
+FillArr(array);
+Console.WriteLine();
+int sum = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    int j = i;
+    sum = sum + array[i, j];
+}
+Console.WriteLine(sum);
+
---
+++ Seminar_8_2/Program.cs
@@ -0,0 +1,82 @@
+﻿/* 
+Решение в группах задач:
+Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая запишет нулями строку и столбец, на пересечении которых расположен наименьший элемент массива.
+Например, задан массив:
+4 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Наименьший элемент - 1, на выходе получим 
+следующий массив:
+0 0 0 0
+0 9 4 2
+0 2 2 6
+0 3 4 7
+*/
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+            //Console.Write(array[i, j] + " ");
+        }
+        //Console.WriteLine();
+    }
+}
+
+// Вывести двумерный массив в консоль в виде матрицы.
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0,3}", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] array)
+{
+    int min = array[0, 0];
+    int minRow = 0; 
+    int minCol = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < min)
+            {
+                min = array[i, j];
+                minRow = i;
+                minCol = j;
+            }
+        }
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[minRow, j] = 0;
+            array[i, minCol] = 0;
+        }
+    }
+}
+
+Console.Write("Write m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Write n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[m, n];
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
+ChangeArray(arr);
+PrintArray(arr);
